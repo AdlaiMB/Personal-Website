74ec0c13b1ba3eb92a7f69805261b95d62b84408
Adlai M.B <adlaimb@gmail.com>
2024-05-28 01:06:51 -0700

    retry on image paths

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import '../styles/About.css';
 function About() {
     return (
         <div className="about-container" id="About">
-            <img className='img-about' src="./Personal-Website/about-image-website.png" alt="" />
+            <img className='img-about' src="Personal-Website/about-image-website.png" alt="" />
             <div className='w-75'>
                 <h2>About Me</h2>
                 <p className='p-about mt-2'>Hey, my name is Adlai and I am currently studying Computer Science at the University of California, Riverside. I’m interested in all things computers (computer architecture, web dev, embedded systems), fútbol, movies, and comic books. I am currently looking for a software engineering internship.   </p>
--- src/components/SkillsItem.js
+++ src/components/SkillsItem.js
@@ -1,7 +1,7 @@
 function SkillsItem({ link, title }) {
     return (
         <div className="skill-item container column center">
-            <img className='img-skills' src={"./Personal-Website/" + link} alt="" />
+            <img className='img-skills' src={"Personal-Website/" + link} alt="" />
             <p className='p-skills'>{title}</p>
         </div>
     );
