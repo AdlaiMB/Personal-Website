ad17f6b2d392af52e855e256a00285d2a812ca2d
Adlai M.B <adlaimb@gmail.com>
2024-05-27 20:30:24 -0700

    skils img sizing fix

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,9 +5,8 @@ function Skills() {
     return (
         <div className="skills-container container column center" id="Skills">
             <h2>My Skills</h2>
-            <div className='container row space-even mt-2 fit-content'>
+            <div className='container row space-even mt-4 fit-content'>
                 <SkillsItem link="/python-icon.svg" title="Python"></SkillsItem>
-                <SkillsItem link="/cplusplus-icon.svg" title="C++"></SkillsItem>
                 <SkillsItem link="/node-icon.svg" title="Node.js"></SkillsItem>
                 <SkillsItem link="/mongo-icon.svg" title="Mongo"></SkillsItem>
                 <SkillsItem link="/git-icon.svg" title="Git"></SkillsItem>
@@ -15,6 +14,7 @@ function Skills() {
                 <SkillsItem link="/css-icon.svg" title="CSS"></SkillsItem>
                 <SkillsItem link="/js-icon.svg" title="JavaScript"></SkillsItem>
                 <SkillsItem link="/arduino-icon.svg" title="Arduino"></SkillsItem>
+                <SkillsItem link="/postgresql-icon.svg" title="PostgreSql"></SkillsItem>
             </div>
         </div>
     );
