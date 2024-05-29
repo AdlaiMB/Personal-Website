d150f9aa9981b5ddfb7b2e128cd82f26c07fa9b4
Adlai Morales-Bravo <adlaimb@gmail.com>
2024-05-29 11:52:56 -0700

    fix more image refs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
         <footer>
             <p className='justify-end'>Copywrite © 2024. All rights reserved.</p>
             <div className='justify-end'>
-                <a href='https://github.com/AdlaiMB' target='_blank' rel='noreferrer'><img className='footer-img' src="/github-icon.svg" alt="" /></a>
+                <a href='https://github.com/AdlaiMB' target='_blank' rel='noreferrer'><img className='footer-img' src="/Personal-Website/github-icon.svg" alt="" /></a>
                 <br></br>
-                <a href='https://linkedin.comhttps://www.linkedin.com/in/adlai-morales-bravo-95561221a/' target='_blank' rel='noreferrer'><img className='footer-img' src="/linkedin-icon.svg" alt="" /></a>
+                <a href='https://linkedin.comhttps://www.linkedin.com/in/adlai-morales-bravo-95561221a/' target='_blank' rel='noreferrer'><img className='footer-img' src="/Personal-Website/linkedin-icon.svg" alt="" /></a>
             </div>
         </footer>
     );
