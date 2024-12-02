0092e2a7be273051b8610a1a5d82f04792fb4273
Adlai M.B <adlaimb@gmail.com>
2024-12-01 22:29:45 -0800

    restart personal website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import './styles/App.css';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Education from './components/Education';
-import Projects from './components/Project';
-import Interests from './components/Interests';
-import Footer from './components/Footer';
-import Skills from './components/Skills';
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Hero></Hero>
-      <About></About>
-      <Education></Education>
-      <Skills></Skills>
-      <Projects></Projects>
-      <Interests></Interests>
-      <Footer></Footer>
+      <p>Hello</p>
     </div>
   );
 }
